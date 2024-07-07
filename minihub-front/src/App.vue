18e4b2d173c9a4d4ef621f05670fbeca2fbbc5ee
t4e1 <jtw5738@naver.com>
2024-07-07 14:20:16 +0900

    07.07 Feat
    1. 스크래핑용 FastAPI 프로젝트 생성

---
+++ minihub-front/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ minihub-front/src/router/router.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            component: () => import('../views/Main/Main.vue')
+        }
+    ]
+});
+
+export default router;
