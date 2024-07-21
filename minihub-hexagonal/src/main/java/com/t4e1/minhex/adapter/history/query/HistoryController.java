f24f5b4a593de75b9f823144156f1c4b0c9b5aa1
t4e1 <jtw5738@naver.com>
2024-07-22 05:21:40 +0900

    07.22 feat
    1. query 컨트롤러쪽 작성중

--- minihub-hexagonal/src/main/java/com/t4e1/minhex/adapter/history/query/HistoryController.java
+++ minihub-hexagonal/src/main/java/com/t4e1/minhex/adapter/history/query/HistoryController.java
@@ -1,9 +1,13 @@
 package com.t4e1.minhex.adapter.history.query;
 
+import com.t4e1.minhex.adapter.history.ReqVO;
 import com.t4e1.minhex.application.port.in.history.query.HistoryQueryUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController("HistoryQueryController")
 @RequestMapping("/history")
@@ -16,5 +20,11 @@ public class HistoryController {
         this.historyQueryUseCase = historyQueryUseCase;
     }
 
+    @GetMapping("{id}")
+    public ReqVO<List<String>> findHistoryById(@PathVariable("id") long id) {
+
+
+        return null;
+    }
 
 }
--- minihub-hexagonal/src/main/java/com/t4e1/minhex/application/service/history/query/HistoryService.java
+++ minihub-hexagonal/src/main/java/com/t4e1/minhex/application/service/history/query/HistoryService.java
@@ -1,9 +1,19 @@
 package com.t4e1.minhex.application.service.history.query;
 
 import com.t4e1.minhex.application.port.in.history.query.HistoryQueryUseCase;
+import com.t4e1.minhex.application.port.out.history.query.HistoryQueryPort;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("HistoryQueryService")
 public class HistoryService implements HistoryQueryUseCase {
+
+    private final HistoryQueryPort historyQueryPort;
+
+    @Autowired
+    public HistoryService(HistoryQueryPort historyQueryPort) {
+        this.historyQueryPort = historyQueryPort;
+    }
+
 
 }
