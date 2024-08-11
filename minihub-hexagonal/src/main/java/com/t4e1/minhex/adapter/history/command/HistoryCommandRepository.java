8bcd3b909d2817e41b66b769f3695f292cdff3a8
t4e1 <jtw5738@naver.com>
2024-08-11 17:00:07 +0900

    08.11 feat
    1. history add 기능 추가

--- minihub-hexagonal/src/main/java/com/t4e1/minhex/adapter/history/command/HistoryCommandRepository.java
+++ minihub-hexagonal/src/main/java/com/t4e1/minhex/adapter/history/command/HistoryCommandRepository.java
@@ -3,10 +3,12 @@ package com.t4e1.minhex.adapter.history.command;
 import com.t4e1.minhex.adapter.converter.EntityMapper;
 import com.t4e1.minhex.adapter.history.HistoryDTO;
 import com.t4e1.minhex.application.port.out.history.command.HistoryCommandPort;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Slf4j
 public class HistoryCommandRepository implements HistoryCommandPort {
 
     private final HistoryJpaRepo historyJpaRepo;
@@ -24,7 +26,14 @@ public class HistoryCommandRepository implements HistoryCommandPort {
     public boolean addRecord(HistoryDTO inputData) {
 
         HistoryEntity data = entityMapper.historyEntity(inputData);
-        historyJpaRepo.save(data);
+
+        try {
+            historyJpaRepo.save(data);
+            return true;
+        } catch(Exception e) {
+            log.info(e.getMessage());
+            return false;
+        }
     }
 
 }
